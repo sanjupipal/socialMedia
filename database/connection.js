2bc00e17ce0fb3c68d89c154bf481c2453b94b03
_Sanju <sanju.pipal@dhwaniris.com>
2022-01-23 13:32:15 +0530

    connection changed

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,21 @@
 const { Sequelize } = require("sequelize");
 (() => {
   // connect Db
-  const db = new Sequelize(process.env.CONNECTION_STRING);
-
+  // const db = new Sequelize(process.env.CONNECTION_STRING);
+  const db = new Sequelize({
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
   //verify connection
   db.authenticate()
     .then(() => {
